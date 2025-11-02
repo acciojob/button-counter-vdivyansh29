9739bde63537c7d890bc0fb1f9b67401a3ca6a9f
Divyansh Verma <divyanshmotog5g@gmail.com>
2025-11-03 00:42:18 +0530

    Add Counter component with click functionality

---
+++ src/components/Counter.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+export default function Counter(){
+  
+    const [count, setCount] = useState(0)
+  
+  return (
+    <div>
+        {/* Do not remove the main div */}
+        <p>Button clicked {count} times</p>
+        <button onClick(() => {
+            setCount(count + 1)
+        })>Click me</button>
+    </div>
+  )
+}
